716f48f7b1a5dbe052e90017e445ca29eefd4971
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-12-12 12:33:48 +0000

    fix; factory address

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ if [ -z "$initCodeHash" ]; then
   exit 1
 fi
 # $ cargo run --release $FACTORY $CALLER $INIT_CODE_HASH <rest of the arguments>
-cargo run --release "0xa6190aBC82427800935E0598892f7488a7F73A04" "0x0000000000000000000000000000000000000000" "$@"
+cargo run --release "0xfAc70910CdD682dF4855AB2b16FB00994cb58889" "0x0000000000000000000000000000000000000000" "$@"
